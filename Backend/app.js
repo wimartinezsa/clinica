e3069f7eec41b55cdc82e4825ae30bb0c129373c
wimartinezsa <180329990+wimartinezsa@users.noreply.github.com>
2024-10-06 01:31:40 +0200

    tabla tipo procedimiento

--- Backend/app.js
+++ Backend/app.js
@@ -2,18 +2,15 @@ import express from "express";
 import cors from "cors";
 import usuario from './src/routers/route.usuario.js'
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(usuario)
-
-
-
+app.use(usuario);
 
 app.listen(port, () => {
     console.log("Listening on port ", port);
-})
+});
