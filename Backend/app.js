491685a502b2c8b5f5e6ea1dc80468a29a885e1a
wimartinezsa <180329990+wimartinezsa@users.noreply.github.com>
2024-10-08 05:06:33 +0200

    modulo de tipo de servicio

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import usuario from './src/routers/route.usuario.js'
+import tipo_servicio from './src/routers/route.tipo.servicio.js'
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(usuario);
+app.use(tipo_servicio);
+
 
 app.listen(port, () => {
     console.log("Listening on port ", port);
